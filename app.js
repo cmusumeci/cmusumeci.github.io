fa8d7e51060835cee3364538d2a3fdb772cfec19
Claudio <cmusumeci89@gmail.com>
2017-05-15 22:45:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-$(function(){$(".page-scroll").bind("click",function(a){var b=$(this);$("html, body").stop().animate({scrollTop:$(b.attr("href")).offset().top-340},1200,"easeInOutExpo"),a.preventDefault()})});
+/* $(function(){$(".page-scroll").bind("click",function(a){var b=$(this);$("html, body").stop().animate({scrollTop:$(b.attr("href")).offset().top-340},1200,"easeInOutExpo"),a.preventDefault()})});
 $(function () {
     $(".custom-close").on('click', function() {
         $('#myModal').modal('hide');
     });
 });
-/*function myFunction() {
+function myFunction() {
     myVar = setTimeout(showPage, 1000);
 }
 
